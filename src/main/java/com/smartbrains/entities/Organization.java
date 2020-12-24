a621dcf6448d1098d2636a4c7527d38459ee7d31
Jimmy Hurff <jhurff@brightmove.com>
2020-12-24 06:59:53 -0600

    Incremental commit

---
+++ src/main/java/com/smartbrains/entities/Organization.java
@@ -0,0 +1,31 @@
+package com.smartbrains.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private final String name;
+    private final String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
